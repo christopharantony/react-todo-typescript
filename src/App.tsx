7ab7b44e26ffd889d3413df8b174d27387bef258
Christophar Antony <christopharantony@gmail.com>
2025-02-09 14:27:01 +0530

    initialize React + TypeScript + Vite project with basic configuration and components

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Todos from "./components/Todos"
+
+
+function App() {
+
+  return (
+    <div>
+      <Todos items={['Learn React', 'Learn TypeScript', 'Live as a Master']} />
+    </div>
+  )
+}
+
+export default App
